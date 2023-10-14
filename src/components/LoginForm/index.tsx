85a60bc976a07525c64071f1deddfb2b1a0129b9
dongmin3891 <rlaehdals753@naver.com>
2023-10-15 00:45:26 +0900

    feat: 회원가입 react-hook-form 적용

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,39 +1,60 @@
+"use client"
 import { tempAuthLogin } from "@/services/MemberService"
 import Link from "next/link"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import RegisterForm from "../Register/RegisterForm";
 
 export default function LoginForm() {
 
-  useEffect(() => {
-    memberJoin();
-  }, [])
+  const [userInfo, setUserInfo] = useState({
+    serviceId: '',
+    email: '',
+    nickname: '',
+    password: '',
+  })
 
-  const memberJoin = async () => {
-    const response = await tempAuthLogin();
-    console.log("response", response);
+  // useEffect(() => {
+  //   memberJoin();
+  // }, [])
+
+  // const memberJoin = async () => {
+  //   const response = await tempAuthLogin();
+  //   console.log("response", response);
+  // }
+
+  //TODO: any형식 수정
+  const handleSubmit = async (event:any) => {
+    event.preventdefault();
+    const formData = new FormData(event.target);
+    const newUser = {
+      serviceId: formData.get('serviceId'),
+      email: formData.get('email'),
+      nickname: formData.get('NickName'),
+      password: formData.get('password'),
+    }
+    console.log("newUser ===>", newUser);
   }
 
   return (
-    <div>
-      <div>
-        <span>아이디</span>
-        <input type="text"></input>
-      </div>
-      <div>
-        <span>이메일</span>
-        <input type="text"></input>
-      </div>
-      <div>
-        <span>닉네임</span>
-        <input type="text"></input>
-      </div>
-      <div>
-        <span>비밀번호</span>
-        <input type="text"></input>
-      </div>
-     
-      <button>회원가입</button>
-
-    </div>
+    <RegisterForm/>
+    // <form onSubmit={(e) => handleSubmit(e)}>
+    //   <label htmlFor="serviceId">Id:</label>
+    //   <input type="text" name="serviceId" />
+    //   <br />
+    //   <label htmlFor="email">Email:</label>
+    //   <input type="email" name="email" />
+    //   <br />
+    //   <label htmlFor="nickname">NickName:</label>
+    //   <input type="text" name="nickname" />
+    //   <br />
+    //   <label htmlFor="password">Password:</label>
+    //   <input type="text" name="password" />
+    //   <br />
+    //   {/* <button type="submit" disabled={isLoading}> */}
+    //   <button type="submit" >
+    //     Create user
+    //   </button>
+    //   {/* {error && <p>Error: {error.message}</p>} */}
+    // </form>
   )
 }
