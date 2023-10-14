a09c5a2e44d82fa32a491f01bc75cd5505cc0837
dongmin3891 <rlaehdals753@naver.com>
2023-10-15 00:46:22 +0900

    fix: 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1>HOME</h1>
-      <Link href="/test"><h2>테스트 회원가입 page이동</h2></Link>
+      <Link href="/test">
+        <h2>테스트 회원가입 page이동</h2>
+      </Link>
     </div>
-  )
-}
+  );
+};
 export default Home;
--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,12 +1,13 @@
-import { ajaxPost } from './BaseService';
+import userInfo from "@/interfaces/models/login/MemberData";
+import { ajaxPost } from "./BaseService";
 
 //TODO: type 설정
-export const tempAuthLogin = async () => {
-  const response = await ajaxPost('/members/sign-in', {
-    serviceId: 'test',
-    email: 'testEmail@naver.com',
-    nickname: 'dongmin',
-    password: 'asdfqwer1234~',
+export const tempAuthLogin = async (param: userInfo) => {
+  const response = await ajaxPost("/members/sign-up", {
+    serviceId: "test",
+    email: "testEmail@naver.com",
+    nickname: "dongmin",
+    password: "asdfqwer1234~",
   });
   return response;
 };
