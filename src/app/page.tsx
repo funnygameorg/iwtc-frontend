893e9ca7dbe06db12c2ccd083f6401893f51d66d
dongmin3891 <rlaehdals753@naver.com>
2023-10-16 20:02:49 +0900

    feat: 검색바 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import Sidebar from "@/components/common/sidebar";
-import Link from "next/link";
+import Sidebar from '@/components/common/Sidebar';
+import SearchBar from '@/components/searsch';
+import Link from 'next/link';
 
 const Home = () => {
-  return (
-    <div>
-      <Sidebar />
-      <h1>HOME</h1>
-      <Link href="/test">
-        <h2>테스트 회원가입 page이동</h2>
-      </Link>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex">
+                <Sidebar />
+                <SearchBar />
+                <div className="line absolute mt-20 transform  h-0.5 bg-gray-200 w-full"></div>
+            </div>
+
+            {/* <div className="ml-64"> TAB</div> */}
+            <Link href="/test">
+                <h2>테스트 회원가입 page이동</h2>
+            </Link>
+        </div>
+    );
 };
 export default Home;
--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+//https://flowbite.com/docs/components/sidebar/#sidebar-with-navbar
 
 function Sidebar() {
     return (
         <div>
+            {/* // -translate-x-full side 숨기기 */}
             <aside
                 id="logo-sidebar"
-                className="fixed left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+                className="left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
                 aria-label="Sidebar"
             >
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
