df890ba835f25677d5fb85a3e9a16edcf7225f26
dongmin3891 <rlaehdals753@naver.com>
2023-10-17 17:51:41 +0900

    feat: 테스트용 로그인 페이지, 회원가입 페이지 추가

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/components/Register/LoginForm";
+import React from "react";
+
+const SignIn = () => {
+  return <LoginForm />;
+};
+
+export default SignIn;
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/Register/RegisterForm";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <RegisterForm />;
+    </div>
+  );
+};
+
+export default SignUp;
