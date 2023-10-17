348c6da5597741f70d72a055e48b65e5c7b698ae
dongmin3891 <rlaehdals753@naver.com>
2023-10-17 17:53:02 +0900

    fix: 코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
-import RankSelect from '@/components/button/RankSelect';
-import Sidebar from '@/components/common/Sidebar';
-import Order from '@/components/dropdown/Order';
-import SearchBar from '@/components/searsch';
-import Link from 'next/link';
+import RankSelect from "@/components/button/RankSelect";
+import Sidebar from "@/components/common/sidebar";
+import Order from "@/components/dropdown/Order";
+import SearchBar from "@/components/searsch";
+import Link from "next/link";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="flex">
-                <Sidebar />
-                <div className="flex">
-                    <SearchBar />
-                    <RankSelect />
-                    <Order />
-                </div>
-                <div className="z-10 line absolute mt-20 transform  h-0.5 bg-gray-200 w-full"></div>
-            </div>
-
-            {/* <div className="ml-64"> TAB</div> */}
-            <Link href="/test">
-                <h2>테스트 회원가입 page이동</h2>
-            </Link>
+  return (
+    <div>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex">
+          <SearchBar />
+          <RankSelect />
+          <Order />
         </div>
-    );
+        <div className="z-10 line absolute mt-20 transform  h-0.5 bg-gray-200 w-full"></div>
+      </div>
+    </div>
+  );
 };
 export default Home;
