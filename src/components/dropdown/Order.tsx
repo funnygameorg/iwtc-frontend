dfce9a35fbbd7b3bc17fc7130cda3c8ed0d15e8c
dongmin3891 <rlaehdals753@naver.com>
2023-10-18 14:26:28 +0900

    fix: order 컴포넌트 ui 수정

--- src/components/dropdown/Order.tsx
+++ src/components/dropdown/Order.tsx
@@ -1,60 +1,63 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 const Order = () => {
-    const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-    const handleDropDown = () => {
-        setOpen(!isOpen);
-    };
+  const handleDropDown = () => {
+    setOpen(!isOpen);
+  };
 
-    return (
-        <div className="ml-4 mt-3 w-200">
-            <button
-                id="dropdownDefaultButton"
-                data-dropdown-toggle="dropdown"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-                onClick={handleDropDown}
+  return (
+    <div className="ml-4 mt-3 w-200">
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        className="text-gray-400 bg-gray-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        onClick={handleDropDown}
+      >
+        최신순
+        <svg
+          className="w-2.5 h-2.5 ml-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+
+      {/* // <!-- Dropdown menu --> */}
+      <div
+        id="dropdown"
+        className={`z-50 ${
+          isOpen ? "block" : "hidden"
+        } bg-white divide-y relative divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 `}
+      >
+        <ul
+          className="py-2 z-50 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-                최신순
-                <svg
-                    className="w-2.5 h-2.5 ml-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m1 1 4 4 4-4"
-                    />
-                </svg>
-            </button>
-
-            {/* // <!-- Dropdown menu --> */}
-            <div
-                id="dropdown"
-                className={`z-50 ${
-                    isOpen ? 'block' : 'hidden'
-                } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 `}
-            >
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                        <a
-                            href="#"
-                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        >
-                            인기순
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+              인기순
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
