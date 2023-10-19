f6b44c06716c0aa8f1fb55fabed89f0cb5ef3857
dongmin3891 <rlaehdals753@naver.com>
2023-10-20 00:10:44 +0900

    feat: logout api 추가

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,13 +1,18 @@
-import { SignUpInfo, SignInInfo } from "@/interfaces/models/login/MemberData";
-import { ajaxPost } from "./BaseService";
+import { SignUpInfo, SignInInfo } from '@/interfaces/models/login/MemberData';
+import { ajaxGet, ajaxPost } from './BaseService';
 
 //TODO: type 설정
 export const userSignUp = async (param: SignUpInfo) => {
-  const response = await ajaxPost("/members/sign-up", param);
-  return response;
+    const response = await ajaxPost('/members/sign-up', param);
+    return response;
 };
 
 export const userSignIn = async (param: SignInInfo) => {
-  const response = await ajaxPost("/members/sign-in", param);
-  return response;
+    const response = await ajaxPost('/members/sign-in', param);
+    return response;
+};
+
+export const userLogOut = async () => {
+    const response = await ajaxGet('/members/sign-out');
+    return response;
 };
