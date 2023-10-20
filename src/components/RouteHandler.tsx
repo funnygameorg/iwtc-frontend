5eabce2dfdc77a35231657929333ec7c39291066
dongmin3891 <rlaehdals753@naver.com>
2023-10-20 23:14:32 +0900

    fix: 임시처리

--- src/components/RouteHandler.tsx
+++ src/components/RouteHandler.tsx
@@ -1,23 +1,22 @@
-'use client'
-import React, { useEffect } from 'react'
-import { useRouter, usePathname } from 'next/navigation'
+'use client';
+import React, { useEffect } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
 import { getAccessToken } from '@/utils/TokenManager';
 const RouteHandler = () => {
+    const router = useRouter();
+    const pathname = usePathname();
 
-  const router = useRouter();
-  const pathname = usePathname();
+    // useEffect(() => {
+    //   if(!getAccessToken()){
+    //     router.push('/');
+    //   }
+    //   // if(getAccessToken())
+    //   // 쿠키 체크!
+    //   // 있으면
+    //   // 없으면 login 페이지 redirect
+    // }, [pathname, router])
 
-  useEffect(() => {
-    if(!getAccessToken()){
-      router.push('/');
-    }
-    // if(getAccessToken())
-    // 쿠키 체크! 
-    // 있으면
-    // 없으면 login 페이지 redirect
-  }, [pathname, router])
-
-  return null;
-}
+    return null;
+};
 
 export default RouteHandler;
--- src/components/header/SignInUpButton.tsx
+++ src/components/header/SignInUpButton.tsx
@@ -1,14 +1,17 @@
-'use client'
-import { userSignOut } from '@/services/MemberService'
-import { getAccessToken } from '@/utils/TokenManager'
-import { useMutation } from '@tanstack/react-query'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+'use client';
+import { userSignOut } from '@/services/MemberService';
+import { getAccessToken } from '@/utils/TokenManager';
+import { useMutation } from '@tanstack/react-query';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const SignInUpButton = () => {
-  const [isLogin, setIsLogin] = useState(getAccessToken() ? true : false)
+    const router = useRouter();
 
-  const { mutate } = useMutation(userSignOut, {
+    const [isLogin, setIsLogin] = useState(getAccessToken() ? true : false);
+
+    const { mutate } = useMutation(userSignOut, {
         onSuccess: (data) => {
             console.log('로그아웃 성공', data.headers);
         },
@@ -17,34 +20,33 @@ const SignInUpButton = () => {
         },
     });
 
-  useEffect(() => {
-    if(getAccessToken()){
-      setIsLogin(true)
-    }else {
-      setIsLogin(false)
-    }
+    useEffect(() => {
+        if (getAccessToken()) {
+            setIsLogin(true);
+        } else {
+            setIsLogin(false);
+        }
+    }, [isLogin]);
 
-  }, [isLogin])
+    const onClickHandler = () => {
+        if (isLogin) {
+            userSignOut();
+        } else {
+            router.push('/sign-in');
+        }
+    };
 
-  const onClickHandler = () => {
-    if(isLogin){  
+    // TODO: 로그인정보 확인
+    return (
+        <div onClick={onClickHandler}>
+            <a
+                href="javascript:"
+                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+                {isLogin ? 'Logout' : 'Login'}
+            </a>
+        </div>
+    );
+};
 
-
-      // 로그아웃 Api 호출
-    }  
-  }
-
-  // TODO: 로그인정보 확인
-  return (
-    <div >
-        <Link
-            href="/sign-in"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-        >
-            {isLogin ? 'Logout' : 'Login'}
-        </Link>
-    </div>
-  )
-}
-
-export default SignInUpButton
+export default SignInUpButton;
