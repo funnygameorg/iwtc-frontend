3c49bece36fcc0552cf53a9f0534f1f723675c83
dongmin3891 <rlaehdals753@naver.com>
2023-10-20 23:19:51 +0900

    feat: 로그아웃 api 추가

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,5 +1,7 @@
 import { SignUpInfo, SignInInfo } from '@/interfaces/models/login/MemberData';
 import { ajaxGet, ajaxPost } from './BaseService';
+import axios from 'axios';
+import { getAccessToken } from '@/utils/TokenManager';
 
 //TODO: type 설정
 export const userSignUp = async (param: SignUpInfo) => {
@@ -13,6 +15,21 @@ export const userSignIn = async (param: SignInInfo) => {
 };
 
 export const userSignOut = async () => {
-    const response = await ajaxGet('/members/sign-out',);
+    const response = await ajaxGet('/members/sign-out');
     return response;
+};
+
+export const userSignOut2 = async () => {
+    const headers = {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${getAccessToken()}`,
+    };
+
+    const response = await axios.get('http://localhost:8080/api/member/sign-out', {
+        headers: headers,
+        timeout: 5000,
+    });
+    if (response) {
+        console.log('response', response);
+    }
 };
