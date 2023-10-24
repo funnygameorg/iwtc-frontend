3a4709415b5fd814317ca8a9c6b68b8c6a927e48
dongmin3891 <rlaehdals753@naver.com>
2023-10-24 23:08:37 +0900

    feat: image domain 설정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       destination: `http://localhost:3000/:path*`,
-  //     },
-  //   ];
-  // },
-  trailingSlash: false,
+    images: { domains: ['picsum.photos'] },
+    // async rewrites() {
+    //   return [
+    //     {
+    //       source: "/:path*",
+    //       destination: `http://localhost:3000/:path*`,
+    //     },
+    //   ];
+    // },
+    trailingSlash: false,
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
