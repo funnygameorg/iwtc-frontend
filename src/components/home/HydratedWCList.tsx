1e5924d969ace2a7a1248b0240394f31411ccfd5
dongmin3891 <rlaehdals753@naver.com>
2023-10-29 15:00:37 +0900

    refactor: 코드정리

--- src/components/home/HydratedWCList.tsx
+++ src/components/home/HydratedWCList.tsx
@@ -1,25 +1,27 @@
-import { dehydrate, Hydrate } from '@tanstack/react-query'
-import getQueryClient from '@/app/getQueryClient'
-import { worldCupAllList } from '@/services/WorldCupService'
-import WorldCup from './worldcup/WorldCupWrapper'
-import RankSelect from '../button/RankSelect'
-import SearchBar from '../search'
-import Order from '../dropdown/Order'
+import { dehydrate, Hydrate } from '@tanstack/react-query';
+import getQueryClient from '@/app/getQueryClient';
+import { worldCupAllList } from '@/services/WorldCupService';
+import WorldCup from './worldcup/WorldCupWrapper';
+import RankSelect from '../button/RankSelect';
+import SearchBar from '../search';
+import Order from '../dropdown/Order';
 
 export default async function HydratedWCList() {
-  const queryClient = getQueryClient()
-  await queryClient.prefetchInfiniteQuery(['wclist','id',undefined,'ALL'], async () => await worldCupAllList(0 ,20, 'id', undefined, 'ALL'))
-  // queryClient.setQueryData(queryKey, (data) => ({
-  //   ...data,
-  //   pageParams: [],
-  // }));
-  const dehydratedState = JSON.parse(JSON.stringify(dehydrate(queryClient)))
-  // console.log("dehydratedState",dehydratedState.queries[0].state.data.pages);
-  
-  dehydratedState.queries[0].state.data.pageParams[0]= 0;
-  return (
-    <Hydrate state={dehydratedState}>
-      <WorldCup />
-    </Hydrate>
-  )
+    const queryClient = getQueryClient();
+    await queryClient.prefetchInfiniteQuery(
+        ['wclist', 'id', undefined, 'ALL'],
+        async () => await worldCupAllList(0, 20, 'id', undefined, 'ALL')
+    );
+    // queryClient.setQueryData(queryKey, (data) => ({
+    //   ...data,
+    //   pageParams: [],
+    // }));
+    const dehydratedState = JSON.parse(JSON.stringify(dehydrate(queryClient)));
+    // console.log("dehydratedState",dehydratedState.queries[0].state.data.pages);
+    dehydratedState.queries[0].state.data.pageParams[0] = 0;
+    return (
+        <Hydrate state={dehydratedState}>
+            <WorldCup />
+        </Hydrate>
+    );
 }
