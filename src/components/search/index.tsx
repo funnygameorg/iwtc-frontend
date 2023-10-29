5bf4d6e9d184bad56702345fdc0fe29ad4b00be5
dongmin3891 <rlaehdals753@naver.com>
2023-10-29 12:57:04 +0900

    feat: 검색시 Enter 이벤트 추가

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,31 +1,35 @@
-'use client'
+'use client';
 import React, { Dispatch, SetStateAction, useState } from 'react';
 
 interface IProps {
-  setKeyword: Dispatch<SetStateAction<undefined | string>>
+    setKeyword: Dispatch<SetStateAction<undefined | string>>;
 }
 
-const SearchBar = ({setKeyword} : IProps) => {
+const SearchBar = ({ setKeyword }: IProps) => {
     const [text, setText] = useState('');
 
     const onChangeKeyword = (e) => {
-      const {value} = e.target
+        const { value } = e.target;
 
-      setText(value ? value : undefined);
-    }
+        setText(value ? value : undefined);
+    };
 
     const onClickSearch = () => {
-      console.log("click!!");
-      setKeyword(text);
-    }
+        setKeyword(text);
+    };
+    const handleOnKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            onClickSearch();
+        }
+    };
 
     return (
         <div className="ml-4 w-200">
-            <label  htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
                 Search
             </label>
             <div className="relative">
-                <div className="absolute right-0 inset-y-0 flex items-center pr-3 " onClick={onClickSearch}  >
+                <div className="absolute right-0 inset-y-0 flex items-center pr-3 " onClick={onClickSearch}>
                     <svg
                         className="w-4 h-4 text-gray-500 dark:text-gray-400 "
                         aria-hidden="true"
@@ -49,6 +53,7 @@ const SearchBar = ({setKeyword} : IProps) => {
                     placeholder="키워드를 검색하세요."
                     required
                     onChange={(e) => onChangeKeyword(e)}
+                    onKeyPress={(e) => handleOnKeyPress(e)}
                     value={text}
                 />
                 {/* <button
