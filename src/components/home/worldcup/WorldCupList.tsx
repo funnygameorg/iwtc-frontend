5919d7518286e4c4fce073c8df126ff741fbcb5d
dongmin3891 <rlaehdals753@naver.com>
2023-11-02 01:07:23 +0900

    fix: 임시 게임화면

--- src/components/home/worldcup/WorldCupList.tsx
+++ src/components/home/worldcup/WorldCupList.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useQueryGetWorldCupAllList, worldCupAllList } from '@/services/WorldCupService';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect } from 'react';
 
 const WorldCupList = ({ wcList }: any) => {
@@ -9,26 +10,28 @@ const WorldCupList = ({ wcList }: any) => {
     return (
         <div className="p-4 max-w-sm">
             <div className="w-80 h-128 rounded overflow-hidden shadow-lg">
-                <div className="flex">
-                    <div className="flex-1">
-                        <Image
-                            className="w-full h-52"
-                            src={reftImgPath}
-                            width={'50'}
-                            height={'10'}
-                            alt={reftContentName}
-                        />
+                <Link href="/play-game">
+                    <div className="flex">
+                        <div className="flex-1">
+                            <Image
+                                className="w-full h-52"
+                                src={reftImgPath}
+                                width={'50'}
+                                height={'10'}
+                                alt={reftContentName}
+                            />
+                        </div>
+                        <div className="flex-1">
+                            <Image
+                                className="w-full h-52"
+                                src={rightImgPath}
+                                width={'50'}
+                                height={'10'}
+                                alt={rightContentName}
+                            />
+                        </div>
                     </div>
-                    <div className="flex-1">
-                        <Image
-                            className="w-full h-52"
-                            src={rightImgPath}
-                            width={'50'}
-                            height={'10'}
-                            alt={rightContentName}
-                        />
-                    </div>
-                </div>
+                </Link>
                 <div className="px-6 py-4 h-52">
                     <div className="font-bold text-xl mb-2">{gameTitle}</div>
                     <p className="text-gray-700 text-base">{description}</p>
