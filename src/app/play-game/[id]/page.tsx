c7f9201a355f6222597b9f17343c3c49c99609fb
dongmin3891 <rlaehdals753@naver.com>
2023-11-08 01:53:57 +0900

    feat: 월드컵 게임 진행 기능 임시 추가

--- src/app/play-game/[id]/page.tsx
+++ src/app/play-game/[id]/page.tsx
@@ -4,18 +4,23 @@ import Image from 'next/image';
 import { useQueryGetWorldCupGameRound, worldCupGamePlay } from '@/services/WorldCupService';
 import RoundPopup from '@/components/popup/RoundPopup';
 import { useMutation } from '@tanstack/react-query';
+import { getEncodedArray } from '@/utils/common';
 
 const Page = ({ params }: { params: { id: number } }) => {
     const { id } = params;
     const { data: roundList } = useQueryGetWorldCupGameRound(id);
     // const {worldCupTitle} = roundList?.data
     const [selectRound, setSelectRound] = useState<number>(0);
+    const [isPlay, setIsPlay] = useState<boolean>(false);
+    const [gameList, setGameList] = useState<any>([]);
+    const [saveClickContents, setSaveClickContents] = useState([]);
 
-    const gameList:any = useMutation(worldCupGamePlay, {
-      onSuccess: (data: any) => {
-        console.log("data", data);
-      }
-    })
+    const getGame: any = useMutation(worldCupGamePlay, {
+        onSuccess: (data: any) => {
+            setIsPlay(true);
+            setGameList(data.data.contentsList);
+        },
+    });
 
     useEffect(() => {
         document.documentElement.classList.add('dark');
@@ -27,52 +32,83 @@ const Page = ({ params }: { params: { id: number } }) => {
         };
     }, []);
 
-    const playMutation = 
-      () => {
+    useEffect(() => {
+        if (gameList) {
+            console.log('gameList', gameList);
+        }
+    }, [gameList]);
+
+    const handleSelection = (index: number) => {
+        // selectRound가 2이면 결승
+        setSaveClickContents(saveClickContents.concat(gameList[index].name));
+
+        if (selectRound === 2) {
+            // 최종 선택 API 호출 후 return
+        }
+        // 배열을 2개씩 자름 결국 2개 남았을 때 클릭 하면 다음 라운드 진출
+        if (gameList.length === 2) {
+            setSelectRound((prev) => prev / 2);
+            return;
+        }
+        //클릭한 아이템은 저장!
+        const newGameList = gameList.slice(2);
+        setGameList(newGameList);
+    };
+
+    const playMutation = () => {
         const param = {
-          worldcupId: id,
-          currentRound: selectRound,
-          divideContentsSizePerRequest: 2,
-          alreadyPlayedContentsIds: undefined
+            worldcupId: id,
+            currentRound: selectRound,
+            divideContentsSizePerRequest: 1,
+            alreadyPlayedContentsIds: saveClickContents.length < 1 ? undefined : saveClickContents.join('&'),
         };
-        gameList.mutate(param);
-      }
-      
-    ;
+        getGame.mutate(param);
+    };
 
-    // useEffect(() => {
-    //     if (selectRound) {
-    //         playMutation();
-    //     }
-    // }, [selectRound]);
+    useEffect(() => {
+        if (selectRound) {
+            playMutation();
+        }
+    }, [selectRound]);
 
-    
-    
+    useEffect(() => {
+        console.log('saveClickContents', saveClickContents);
+    }, [saveClickContents]);
+
+    // const handleSelection = (index) => {
+    //     setSelectedIndex(index);
+    // };
+
+    if (!isPlay) {
+        return <RoundPopup roundList={roundList} setSelectRound={setSelectRound} />;
+    }
+
     return (
         <>
-            <RoundPopup roundList={roundList} setSelectRound={setSelectRound} />
             <div className="grid h-screen place-items-center box-border">
-            <h1 className='text-white text-3xl'>{roundList?.data?.worldCupTitle}</h1>
+                <h1 className="text-white text-3xl">{roundList?.data?.worldCupTitle}</h1>
+                <h1 className="text-white text-3xl">{selectRound === 2 ? '결승' : selectRound}강</h1>
+
                 <div className="flex p-4 text-black shadow" style={{ width: '1600px', height: '1000px' }}>
-                    <div className="flex items-start">
+                    <div className="flex items-start" onClick={() => handleSelection(0)}>
                         <Image
                             className="h-full w-full"
-                            src={'/images/default.png'}
+                            src={gameList[0].filePath}
                             width={'750'}
                             height={'500'}
-                            alt={''}
+                            alt={gameList[0].name}
                         />
                     </div>
                     <div className="grid place-items-center ">
                         <span>VS</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end" onClick={() => handleSelection(1)}>
                         <Image
                             className="h-full w-full"
-                            src={'/images/default.png'}
+                            src={gameList[1].filePath}
                             width={'750'}
                             height={'500'}
-                            alt={''}
+                            alt={gameList[1].name}
                         />
                     </div>
                 </div>
@@ -83,4 +119,3 @@ const Page = ({ params }: { params: { id: number } }) => {
 };
 
 export default Page;
-// public/images/default.png
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,12 @@
 export const getKeyByValue = (obj: any, value: string) => {
-  if (Object.keys(obj)) return true;
-  return false;
+    if (Object.keys(obj)) return true;
+    return false;
+};
+
+export const getEncodedArray = (myArray) => {
+    const encodedArray = myArray.map((element) => {
+        return encodeURIComponent(element);
+    });
+    console.log('encodedArray', encodedArray);
+    return encodedArray;
 };
