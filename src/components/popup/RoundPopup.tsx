489c50b458b0e9587c062b9da4c0e2a3fbf48c46
rara2 <kma952727@gmail.com>
2023-12-18 18:16:45 +0900

    fix : 게임 라운드 선택창의 "popular" 문구 제거

--- src/components/popup/RoundPopup.tsx
+++ src/components/popup/RoundPopup.tsx
@@ -72,9 +72,6 @@ const RoundPopup = ({ roundList, setSelectRound }: IProps) => {
                                             className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
                                         >
                                             <span className="flex-1 ml-3 whitespace-nowrap">{item}강</span>
-                                            <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
-                                                Popular
-                                            </span>
                                         </a>
                                     </li>
                                 ))}
