f45a9d790128795d332b3b22a6fe549a9404d690
dongmin3891 <rlaehdals753@naver.com>
2023-12-31 17:08:55 +0900

    fix: 결과 페이지 디자인 수정

--- src/components/Rank/RankListWrapper.tsx
+++ src/components/Rank/RankListWrapper.tsx
@@ -19,18 +19,20 @@ const RankListWrapper = ({ contentsId }: IProps) => {
     // );
 
     return (
-        <div className="bg-gray-100 rounded-md shadow-md p-6 mt-12">
-            <h2 className="text-2xl font-bold mb-4">게임 결과 랭킹 목록</h2>
-            {allRankIsSuccess ? (
-                <ul className="divide-y divide-gray-200">
-                    {allRankList.data.map((items: any, index: number) => {
-                        const { contentsName } = items;
-                        return <RankList key={index} contentsName={contentsName} rank={index + 1} />;
-                    })}
-                </ul>
-            ) : (
-                <p>로딩 중...</p>
-            )}
+        <div className="w-full flex flex-col bg-zinc-900 border border-zinc-600 rounded-md shadow-md p-6 mt-2">
+            <div className="h-full">
+                <h2 className="text-2xl font-bold mb-4">게임 결과 랭킹 목록</h2>
+                {allRankIsSuccess ? (
+                    <ul className="divide-y divide-gray-200 w-full h-1/6">
+                        {allRankList.data.map((items: any, index: number) => {
+                            const { contentsName } = items;
+                            return <RankList key={index} contentsName={contentsName} rank={index + 1} />;
+                        })}
+                    </ul>
+                ) : (
+                    <p>로딩 중...</p>
+                )}
+            </div>
         </div>
     );
 };
--- src/components/reply/ReplyList.tsx
+++ src/components/reply/ReplyList.tsx
@@ -24,16 +24,16 @@ const ReplyList = ({ replyData }: IProps) => {
     };
     return (
         <>
-            <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+            <article className="bg-zinc-900 border border-zinc-600 p-6 text-base rounded-lg dark:bg-gray-900">
                 <footer className="flex justify-between items-center mb-2">
                     <div className="flex items-center">
-                        <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                        <p className="inline-flex items-center mr-3 text-sm font-semibold">
                             <img
                                 className="mr-2 w-6 h-6 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
                                 alt="Michael Gough"
                             />
-                            {writerNickname}
+                            <span className="text-yellow-400">{writerNickname}</span>
                         </p>
                         <p className="text-sm text-gray-600 dark:text-gray-400">
                             <time dateTime="2022-02-08" title="February 8th, 2022">
@@ -41,7 +41,7 @@ const ReplyList = ({ replyData }: IProps) => {
                             </time>
                         </p>
                     </div>
-                    <button
+                    {/* <button
                         id={`dropdownComment${commentId}Button`}
                         data-dropdown-toggle={`dropdownComment${commentId}`}
                         className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -58,11 +58,11 @@ const ReplyList = ({ replyData }: IProps) => {
                             <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
                         </svg>
                         <span className="sr-only">Comment settings</span>
-                    </button>
+                    </button> */}
                     {/* <ReplyPopup commentId={commentId} isPopup={isPopup} /> */}
                 </footer>
 
-                <p className="text-gray-500 dark:text-gray-400">{body}</p>
+                <p className="text-white dark:text-gray-400">{body}</p>
             </article>
         </>
     );
