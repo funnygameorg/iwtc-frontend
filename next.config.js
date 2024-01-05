518f751b69992f011550fecf6f46124c855ffa35
dongmin3891 <rlaehdals753@naver.com>
2024-01-06 00:54:26 +0900

    feat: ffmpeg 라이브러리 추가

--- next.config.js
+++ next.config.js
@@ -10,6 +10,24 @@ const nextConfig = {
     //   ];
     // },
     trailingSlash: false,
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/', // change to appropriate path
+                headers: [
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
