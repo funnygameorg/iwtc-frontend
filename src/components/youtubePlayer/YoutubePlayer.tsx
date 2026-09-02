5e97c527ca217c886e811a92bb2fd6d7f9bd4334
dongmin3891 <rlaehdals753@naver.com>
2026-09-02 19:07:33 +0900

    refactor: 유튜브 iframe 컴포넌트 타입 연결

--- src/components/youtubePlayer/YoutubePlayer.tsx
+++ src/components/youtubePlayer/YoutubePlayer.tsx
@@ -1,32 +1,30 @@
-import React, { useContext } from 'react';
 import { getYoutubeVideoId } from '@/utils/youtube';
-import AlertPopup from '../popup/AlertPopup';
-import { PopupContext } from '@/providers/PopupProvider';
 
-const YoutubePlayer = ({ url, componentType }: any) => {
-    const { showPopup, hidePopup } = useContext(PopupContext);
+interface IProps {
+    url?: string;
+    componentType: 'uploadForm' | 'uploadList';
+}
 
+const YoutubePlayer = ({ url, componentType }: IProps) => {
     // 플레이어 사용 위치에 따라 플레이어 크기 선택
-    const getPlayerSize = (componentType: any) => {
+    const getPlayerSize = (componentType: IProps['componentType']): [number, number] => {
         const uploadComponentWidth = 560;
         const uploadComponentHeight = 315;
 
         const uploadListComponentWidth = 400;
         const uploadListComponentHeight = 200;
 
-        if (componentType == 'uploadForm') {
+        if (componentType === 'uploadForm') {
             return [uploadComponentWidth, uploadComponentHeight];
-        } else if (componentType == 'uploadList') {
-            return [uploadListComponentWidth, uploadListComponentHeight];
-        } else {
-            showPopup(<AlertPopup message={'sorry..'} hidePopup={hidePopup} />);
         }
+
+        return [uploadListComponentWidth, uploadListComponentHeight];
     };
 
-    const videoId = getYoutubeVideoId(url);
+    const videoId = getYoutubeVideoId(url || '');
 
     const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-    const [width, height]: any = getPlayerSize(componentType);
+    const [width, height] = getPlayerSize(componentType);
 
     return (
         <div>
